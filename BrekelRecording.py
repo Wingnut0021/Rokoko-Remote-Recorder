60ee482cf847bc7b63df4e369a08ece84c542bf5
Wingnut0021 <92347044+Wingnut0021@users.noreply.github.com>
2023-06-07 15:23:32 +1000

    Include all 6 suits

--- BrekelRecording.py
+++ BrekelRecording.py
@@ -1,7 +1,8 @@
 import socket
+import asyncio
 
-TCP_IP = "192.168.50.52"
-PORT = 8889
+TCP_IP = "127.0.0.1"
+TCP_PORT = 8889
 
 #MESSAGE = b"Brekel_recording_start"
 BUFFER_SIZE = 1024
@@ -13,10 +14,16 @@ BUFFER_SIZE = 1024
 
 
 def brekel_start():
-    MESSAGE = "Brekel_recording_start"
-    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    s.connect((TCP_IP,PORT))
-    s.send(bytes(MESSAGE,'UTF-8'))
+    MESSAGE = b"Brekel_recording_start\tTIMESHIFTVIS"
+    with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
+        s.connect((TCP_IP,TCP_PORT))
+        #s.sendall(MESSAGE)
+        data = s.recv(1024)
+
+    print(f"Received {data!r}")
+
+
+
+    s.close()
     #print (s.recv(BUFFER_SIZE))
     #print("Received data:",data)
-    s.close()
--- main.py
+++ main.py
@@ -3,8 +3,13 @@ import customtkinter
 import rokokoStartRecording as start
 import BrekelRecording as brekel
 
+
 IP_ADDRESS_1 = ""
 IP_ADDRESS_2 = ""
+IP_ADDRESS_3 = ""
+IP_ADDRESS_4 = ""
+IP_ADDRESS_5 = ""
+IP_ADDRESS_6 = ""
 
 def combobox_callback(choice):
     print("combobox dropdown clicked!", choice)
@@ -36,8 +41,8 @@ customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-bl
 
 
 app = customtkinter.CTk()  # create CTk window like you do with the Tk window
-app.geometry("600x600")
-app.title("Rokoko Remote Recorder")
+app.geometry("1000x800")
+app.title("Rokoko Multi Remote Recorder")
 app.grid_rowconfigure(10, weight=1)
 app.grid_columnconfigure(10, weight=1)
 
@@ -46,12 +51,28 @@ def button_startRecordAll():
     start.start_Recording(IP_ADDRESS_1)
     IP_ADDRESS_2 = suit2_Ip.get()
     start.start_Recording(IP_ADDRESS_2)
+    IP_ADDRESS_3 = suit3_Ip.get()
+    start.start_Recording(IP_ADDRESS_3)
+    IP_ADDRESS_4 = suit4_Ip.get()
+    start.start_Recording(IP_ADDRESS_4)
+    IP_ADDRESS_5 = suit5_Ip.get()
+    start.start_Recording(IP_ADDRESS_5)
+    IP_ADDRESS_6 = suit6_Ip.get()
+    start.start_Recording(IP_ADDRESS_6)
 
 def button_stopRecordAll():
     IP_ADDRESS_1 = suit1_Ip.get()
     start.stop_Recording(IP_ADDRESS_1)
     IP_ADDRESS_2 = suit2_Ip.get()
     start.stop_Recording(IP_ADDRESS_2)
+    IP_ADDRESS_3 = suit3_Ip.get()
+    start.stop_Recording(IP_ADDRESS_3)
+    IP_ADDRESS_4 = suit4_Ip.get()
+    start.stop_Recording(IP_ADDRESS_4)
+    IP_ADDRESS_5 = suit5_Ip.get()
+    start.stop_Recording(IP_ADDRESS_5)
+    IP_ADDRESS_6 = suit6_Ip.get()
+    start.stop_Recording(IP_ADDRESS_6)
 
 def calibrate_1():
     IP_ADDRESS_1 = suit1_Ip.get()
@@ -60,10 +81,30 @@ def calibrate_1():
 def calibrate_2():
     IP_ADDRESS_2 = suit2_Ip.get()
     start.calibrate_Suit(IP_ADDRESS_2)
+
+def calibrate_3():
+    IP_ADDRESS_3 = suit3_Ip.get()
+    start.calibrate_Suit(IP_ADDRESS_3)
+
+def calibrate_4():
+    IP_ADDRESS_4 = suit4_Ip.get()
+    start.calibrate_Suit(IP_ADDRESS_4)
+
+def calibrate_5():
+    IP_ADDRESS_5 = suit5_Ip.get()
+    start.calibrate_Suit(IP_ADDRESS_5)
+
+def calibrate_6():
+    IP_ADDRESS_6 = suit6_Ip.get()
+    start.calibrate_Suit(IP_ADDRESS_6)
     
 def calibrate_All():
     calibrate_1()
     calibrate_2()
+    calibrate_3()
+    calibrate_4()
+    calibrate_5()
+    calibrate_6()
 
 def button_startRecordBrekel():
     brekel.brekel_start()
@@ -71,45 +112,107 @@ def button_stopRecordBrekel():
     start.stop_Recording()
 
 # Use CTkButton instead of tkinter Button
-buttonStartAll = customtkinter.CTkButton(master=app, text="Start Recording", command=button_startRecordAll)
-buttonStartAll.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+buttonStartAll = customtkinter.CTkButton(master=app, text="Start Recording All", command=button_startRecordAll)
+buttonStartAll.place(relx=0.7, rely=0.1, anchor=tkinter.CENTER)
 
-buttonStopAll = customtkinter.CTkButton(master=app, text="Stop Recording", command=button_stopRecordAll)
-buttonStopAll.place(relx=0.8, rely=0.5, anchor=tkinter.CENTER)
+buttonStopAll = customtkinter.CTkButton(master=app, text="Stop Recording All", command=button_stopRecordAll)
+buttonStopAll.place(relx=0.9, rely=0.1, anchor=tkinter.CENTER)
 
-buttonStartBrekel = customtkinter.CTkButton(master=app, text="Start Recording Brekel", command=button_startRecordBrekel)
-buttonStartBrekel.place(relx=0.5, rely=0.7, anchor=tkinter.CENTER)
+#buttonStartBrekel = customtkinter.CTkButton(master=app, text="Connect to Brekel", command=button_startRecordBrekel)
+#buttonStartBrekel.place(relx=0.5, rely=0.7, anchor=tkinter.CENTER)
 
-buttonStopBrekel = customtkinter.CTkButton(master=app, text="Stop Recording Brekel", command=button_stopRecordBrekel)
-buttonStopBrekel.place(relx=0.8, rely=0.7, anchor=tkinter.CENTER)
+#buttonStopBrekel = customtkinter.CTkButton(master=app, text="Disconnect Brekel", command=button_stopRecordBrekel)
+#buttonStopBrekel.place(relx=0.8, rely=0.7, anchor=tkinter.CENTER)
 
-combobox = customtkinter.CTkComboBox(app, values=['30', '60', '100'], command=combobox_callback)
+combobox = customtkinter.CTkComboBox(app, values=['60', '100'], command=combobox_callback)
 combobox.place(relx=0.5, rely=0.4, anchor=tkinter.CENTER)
 
-#button = customtkinter.CTkButton(master=app, text="Get input value", command=get_all_inputs)
-#button.place(relx=0.7, rely=0.2, anchor=tkinter.CENTER)
-
+# calibrate suits
 buttonCalibrateAll = customtkinter.CTkButton(master=app, text="Calibrate All Suits", command=calibrate_All)
-buttonCalibrateAll.place(relx=0.8, rely=0.2, anchor=tkinter.CENTER)
+buttonCalibrateAll.place(relx=0.5, rely=0.1, anchor=tkinter.CENTER)
 
 buttonCalibrate1 = customtkinter.CTkButton(master=app, text="Calibrate Suit 1", command=calibrate_1)
 buttonCalibrate1.place(relx=0.5, rely=0.2, anchor=tkinter.CENTER)
 
 buttonCalibrate2 = customtkinter.CTkButton(master=app, text="Calibrate Suit 2", command=calibrate_2)
-buttonCalibrate2.place(relx=0.2, rely=0.2, anchor=tkinter.CENTER)
+buttonCalibrate2.place(relx=0.5, rely=0.3, anchor=tkinter.CENTER)
 
-# Create entry widget for suit 1 IP Address
-suit1_Ip = customtkinter.CTkEntry(app, placeholder_text="Suit 1 IP Address")
-suit1_Ip.place(relx=0.5, rely=0.6, anchor=tkinter.CENTER)
+buttonCalibrate3 = customtkinter.CTkButton(master=app, text="Calibrate Suit 3", command=calibrate_3)
+buttonCalibrate3.place(relx=0.5, rely=0.4, anchor=tkinter.CENTER)
 
-suit2_Ip = customtkinter.CTkEntry(app, placeholder_text="Suit 2 IP Address")
-suit2_Ip.place(relx=0.2, rely=0.6, anchor=tkinter.CENTER)
+buttonCalibrate4 = customtkinter.CTkButton(master=app, text="Calibrate Suit 4", command=calibrate_4)
+buttonCalibrate4.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+
+buttonCalibrate5 = customtkinter.CTkButton(master=app, text="Calibrate Suit 5", command=calibrate_5)
+buttonCalibrate5.place(relx=0.5, rely=0.6, anchor=tkinter.CENTER)
+
+buttonCalibrate6 = customtkinter.CTkButton(master=app, text="Calibrate Suit 6", command=calibrate_6)
+buttonCalibrate6.place(relx=0.5, rely=0.7, anchor=tkinter.CENTER)
+
+# Create entry widget for suit IP Addresses
+suit1_Ip = customtkinter.CTkEntry(app, placeholder_text="0.0.0.0")
+suit1_Ip.place(relx=0.3, rely=0.2, anchor=tkinter.CENTER)
+suit1_Ip_label = customtkinter.CTkLabel(app, text="Suit 1 IP Address", fg_color="transparent")
+suit1_Ip_label.place(relx=0.15, rely=0.2, anchor=tkinter.CENTER)
+suit1_Name = customtkinter.CTkEntry(app, placeholder_text="File Name")
+suit1_Name.place(relx=0.9, rely=0.2, anchor=tkinter.CENTER)
+suit1_Name_label = customtkinter.CTkLabel(app, text="File Name", fg_color="transparent")
+suit1_Name_label.place(relx=0.75, rely=0.2, anchor=tkinter.CENTER)
+
+suit2_Ip = customtkinter.CTkEntry(app, placeholder_text="0.0.0.0")
+suit2_Ip.place(relx=0.3, rely=0.3, anchor=tkinter.CENTER)
+suit2_Ip_label = customtkinter.CTkLabel(app, text="Suit 2 IP Address", fg_color="transparent")
+suit2_Ip_label.place(relx=0.15, rely=0.3, anchor=tkinter.CENTER)
+suit2_Name = customtkinter.CTkEntry(app, placeholder_text="File Name")
+suit2_Name.place(relx=0.9, rely=0.3, anchor=tkinter.CENTER)
+suit2_Name_label = customtkinter.CTkLabel(app, text="File Name", fg_color="transparent")
+suit2_Name_label.place(relx=0.75, rely=0.3, anchor=tkinter.CENTER)
+
+suit3_Ip = customtkinter.CTkEntry(app, placeholder_text="0.0.0.0")
+suit3_Ip.place(relx=0.3, rely=0.4, anchor=tkinter.CENTER)
+suit3_Ip_label = customtkinter.CTkLabel(app, text="Suit 2 IP Address", fg_color="transparent")
+suit3_Ip_label.place(relx=0.15, rely=0.4, anchor=tkinter.CENTER)
+suit3_Name = customtkinter.CTkEntry(app, placeholder_text="File Name")
+suit3_Name.place(relx=0.9, rely=0.4, anchor=tkinter.CENTER)
+suit3_Name_label = customtkinter.CTkLabel(app, text="File Name", fg_color="transparent")
+suit3_Name_label.place(relx=0.75, rely=0.4, anchor=tkinter.CENTER)
+
+suit4_Ip = customtkinter.CTkEntry(app, placeholder_text="0.0.0.0")
+suit4_Ip.place(relx=0.3, rely=0.5, anchor=tkinter.CENTER)
+suit4_Ip_label = customtkinter.CTkLabel(app, text="Suit 4 IP Address", fg_color="transparent")
+suit4_Ip_label.place(relx=0.15, rely=0.5, anchor=tkinter.CENTER)
+suit4_Name = customtkinter.CTkEntry(app, placeholder_text="File Name")
+suit4_Name.place(relx=0.9, rely=0.5, anchor=tkinter.CENTER)
+suit4_Name_label = customtkinter.CTkLabel(app, text="File Name", fg_color="transparent")
+suit4_Name_label.place(relx=0.75, rely=0.5, anchor=tkinter.CENTER)
+
+suit5_Ip = customtkinter.CTkEntry(app, placeholder_text="0.0.0.0")
+suit5_Ip.place(relx=0.3, rely=0.6, anchor=tkinter.CENTER)
+suit5_Ip_label = customtkinter.CTkLabel(app, text="Suit 5 IP Address", fg_color="transparent")
+suit5_Ip_label.place(relx=0.15, rely=0.6, anchor=tkinter.CENTER)
+suit4_Name = customtkinter.CTkEntry(app, placeholder_text="File Name")
+suit4_Name.place(relx=0.9, rely=0.6, anchor=tkinter.CENTER)
+suit4_Name_label = customtkinter.CTkLabel(app, text="File Name", fg_color="transparent")
+suit4_Name_label.place(relx=0.75, rely=0.6, anchor=tkinter.CENTER)
+
+suit6_Ip = customtkinter.CTkEntry(app, placeholder_text="0.0.0.0")
+suit6_Ip.place(relx=0.3, rely=0.7, anchor=tkinter.CENTER)
+suit6_Ip_label = customtkinter.CTkLabel(app, text="Suit 6 IP Address", fg_color="transparent")
+suit6_Ip_label.place(relx=0.15, rely=0.7, anchor=tkinter.CENTER)
+suit6_Name = customtkinter.CTkEntry(app, placeholder_text="File Name")
+suit6_Name.place(relx=0.9, rely=0.7, anchor=tkinter.CENTER)
+suit6_Name_label = customtkinter.CTkLabel(app, text="File Name", fg_color="transparent")
+suit6_Name_label.place(relx=0.75, rely=0.7, anchor=tkinter.CENTER)
 
 api_Key = customtkinter.CTkEntry(app, placeholder_text="1234")
-api_Key.place(relx=0.5, rely=0.8, anchor=tkinter.CENTER)
+api_Key.place(relx=0.3, rely=0.8, anchor=tkinter.CENTER)
+api_Key_label = customtkinter.CTkLabel(app, text="API Key", fg_color="transparent")
+api_Key_label.place(relx=0.15, rely=0.8, anchor=tkinter.CENTER)
 
 port = customtkinter.CTkEntry(app, placeholder_text="80")
-port.place(relx=0.5, rely=0.9, anchor=tkinter.CENTER)
+port.place(relx=0.3, rely=0.9, anchor=tkinter.CENTER)
+api_Key_label = customtkinter.CTkLabel(app, text="Network Port", fg_color="transparent")
+api_Key_label.place(relx=0.15, rely=0.9, anchor=tkinter.CENTER)
 
 #IP_ADDRESS_1 = suit1_Ip.get()
 #IP_ADDRESS_2 = suit2_Ip.get()
