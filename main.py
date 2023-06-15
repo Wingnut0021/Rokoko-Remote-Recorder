eb045ce5f93da1131c824409262a5269ad641197
Wingnut0021 <92347044+Wingnut0021@users.noreply.github.com>
2023-06-15 11:02:14 +1000

    Optimized Record Function

--- main.py
+++ main.py
@@ -19,74 +19,43 @@ class App(customtkinter.CTk):
         image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "images")
         
         def button_startRecordAll():
-            TAKE_NUMBER = take_number.get()
-            now = datetime.now()
-            NOW = now.strftime("%d_%m_%Y_%H_%M_%S")
-            suit1_ready = suit_checkbox1.get()
-            suit2_ready = suit_checkbox2.get()
-            suit3_ready = suit_checkbox3.get()
-            suit4_ready = suit_checkbox4.get()
-            suit5_ready = suit_checkbox5.get()
-            suit6_ready = suit_checkbox6.get()
-            brekel_ready = brekel_checkbox.get()
+            TAKE_NUMBER = take_number.get().zfill(2)
+            PART_NUMBER = part_number.get().zfill(2)
+            NOW = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
             self.status_textbox.insert("0.0", "Recording Started\n")
             self.timer_label.configure(text=time.time)
-            if brekel_ready == 1:
+            if brekel_checkbox.get() == 1:
                 self.brekel_frame.configure(border_color="red", border_width=3)
                 brekel.click_Brekel()
-            if suit1_ready == 1:
+            if suit_checkbox1.get() == 1:
                 self.suit_frame1.configure(border_color="red", border_width=3)
                 IP_ADDRESS = suit_Ip1.get()
-                CLIP_NAME = suit_Name1.get()
-                if int(TAKE_NUMBER) < 10:
-                    CLIP_NAME = CLIP_NAME + "_Take0" + str(TAKE_NUMBER) + "_" + NOW
-                else:
-                    CLIP_NAME = CLIP_NAME + "_Take" + str(TAKE_NUMBER) + "_" + NOW
+                CLIP_NAME = suit_Name1.get() + "_P" + PART_NUMBER + "_Take" + str(TAKE_NUMBER) + "_" + NOW
                 start.start_Recording(IP_ADDRESS,CLIP_NAME)
-            if suit2_ready == 1:
+            if suit_checkbox2.get() == 1:
                 self.suit_frame2.configure(border_color="red", border_width=3)
                 IP_ADDRESS = suit_Ip2.get()
-                CLIP_NAME = suit_Name2.get()
-                if int(TAKE_NUMBER) < 10:
-                    CLIP_NAME = CLIP_NAME + "_Take0" + str(TAKE_NUMBER) + "_" + NOW
-                else:
-                    CLIP_NAME = CLIP_NAME + "_Take" + str(TAKE_NUMBER) + "_" + NOW
+                CLIP_NAME = suit_Name2.get() + "_P" + PART_NUMBER + "_Take" + str(TAKE_NUMBER) + "_" + NOW
                 start.start_Recording(IP_ADDRESS,CLIP_NAME)
-            if suit3_ready == 1:
+            if suit_checkbox3.get() == 1:
                 self.suit_frame3.configure(border_color="red", border_width=3)
                 IP_ADDRESS = suit_Ip3.get()
-                CLIP_NAME = suit_Name3.get()
-                if int(TAKE_NUMBER) < 10:
-                    CLIP_NAME = CLIP_NAME + "_Take0" + str(TAKE_NUMBER) + "_" + NOW
-                else:
-                    CLIP_NAME = CLIP_NAME + "_Take" + str(TAKE_NUMBER) + "_" + NOW
+                CLIP_NAME = suit_Name3.get() + "_P" + PART_NUMBER + "_Take" + str(TAKE_NUMBER) + "_" + NOW
                 start.start_Recording(IP_ADDRESS,CLIP_NAME)
-            if suit4_ready == 1:
+            if suit_checkbox4.get() == 1:
                 self.suit_frame4.configure(border_color="red", border_width=3)
                 IP_ADDRESS = suit_Ip4.get()
-                CLIP_NAME = suit_Name4.get()
-                if int(TAKE_NUMBER) < 10:
-                    CLIP_NAME = CLIP_NAME + "_Take0" + str(TAKE_NUMBER) + "_" + NOW
-                else:
-                    CLIP_NAME = CLIP_NAME + "_Take" + str(TAKE_NUMBER) + "_" + NOW
+                CLIP_NAME = suit_Name4.get() + "_P" + PART_NUMBER + "_Take" + str(TAKE_NUMBER) + "_" + NOW
                 start.start_Recording(IP_ADDRESS,CLIP_NAME)
-            if suit5_ready == 1:
+            if suit_checkbox5.get() == 1:
                 self.suit_frame5.configure(border_color="red", border_width=3)
                 IP_ADDRESS = suit_Ip5.get()
-                CLIP_NAME = suit_Name5.get()
-                if int(TAKE_NUMBER) < 10:
-                    CLIP_NAME = CLIP_NAME + "_Take0" + str(TAKE_NUMBER) + "_" + NOW
-                else:
-                    CLIP_NAME = CLIP_NAME + "_Take" + str(TAKE_NUMBER) + "_" + NOW
+                CLIP_NAME = suit_Name5.get() + "_P" + PART_NUMBER + "_Take" + str(TAKE_NUMBER) + "_" + NOW
                 start.start_Recording(IP_ADDRESS,CLIP_NAME)
-            if suit6_ready == 1:
+            if suit_checkbox6.get() == 1:
                 self.suit_frame6.configure(border_color="red", border_width=3)
                 IP_ADDRESS = suit_Ip6.get()
-                CLIP_NAME = suit_Name6.get()
-                if int(TAKE_NUMBER) < 10:
-                    CLIP_NAME = CLIP_NAME + "_Take0" + str(TAKE_NUMBER) + "_" + NOW
-                else:
-                    CLIP_NAME = CLIP_NAME + "_Take" + str(TAKE_NUMBER) + "_" + NOW
+                CLIP_NAME = suit_Name6.get() + "_P" + PART_NUMBER + "_Take" + str(TAKE_NUMBER) + "_" + NOW
                 start.start_Recording(IP_ADDRESS,CLIP_NAME)
             else:
                 print("No devices selected")
@@ -214,37 +183,43 @@ class App(customtkinter.CTk):
 
         # Configure sidebar frame
         self.sidebar_frame = customtkinter.CTkFrame(self, height=800, width=300, corner_radius=0)
-        self.sidebar_frame.grid(row=0, column=0, rowspan=20, sticky="nsew")
+        self.sidebar_frame.grid(row=0, column=0, rowspan=19, sticky="nsew")
         self.sidebar_frame.grid_rowconfigure(0, weight=1)
         self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="Settings", font=customtkinter.CTkFont(size=20, weight="bold"))
-        self.logo_label.grid(row=1, column=0, padx=20, pady=10)
+        self.logo_label.grid(row=1, column=0, padx=10, pady=0)
 
         scene_Name_label = customtkinter.CTkLabel(self.sidebar_frame, text="Scene Name", fg_color="transparent")
-        scene_Name_label.grid(row=2, column=0, padx=20, pady=10)
+        scene_Name_label.grid(row=2, column=0, padx=10, pady=0)
         scene_Name = customtkinter.CTkEntry(self.sidebar_frame, placeholder_text="")
-        scene_Name.grid(row=3, column=0, rowspan=1, sticky="nsew", padx=20, pady=10)
+        scene_Name.grid(row=3, column=0, rowspan=1, sticky="nsew", padx=10, pady=0)
         
         take_number_label = customtkinter.CTkLabel(self.sidebar_frame, text="Take Number", fg_color="transparent")
-        take_number_label.grid(row=4, column=0, padx=20, pady=10)
+        take_number_label.grid(row=4, column=0, padx=10, pady=0)
         take_number = customtkinter.CTkEntry(self.sidebar_frame, placeholder_text="")
-        take_number.grid(row=5, column=0, rowspan=1, sticky="nsew", padx=20, pady=10)
+        take_number.grid(row=5, column=0, rowspan=1, sticky="nsew", padx=10, pady=0)
         take_number.insert(0, 1)
-        
+
+        part_number_label = customtkinter.CTkLabel(self.sidebar_frame, text="Part Number", fg_color="transparent")
+        part_number_label.grid(row=6, column=0, padx=10, pady=0)
+        part_number = customtkinter.CTkEntry(self.sidebar_frame, placeholder_text="")
+        part_number.grid(row=7, column=0, rowspan=1, sticky="nsew", padx=10, pady=0)
+        part_number.insert(0, 1)
+
         frameRate_label = customtkinter.CTkLabel(self.sidebar_frame, text="Frame Rate", fg_color="transparent")
-        frameRate_label.grid(row=6, column=0, padx=20, pady=10)
+        frameRate_label.grid(row=8, column=0, padx=10, pady=0)
         frameRate = customtkinter.CTkComboBox(self.sidebar_frame, values=['60', '100'])
-        frameRate.grid(row=7, column=0, padx=20, pady=10)
+        frameRate.grid(row=9, column=0, padx=10, pady=0)
 
         api_Key_label = customtkinter.CTkLabel(self.sidebar_frame, text="API Key", fg_color="transparent")
-        api_Key_label.grid(row=8, column=0, padx=20, pady=10)
+        api_Key_label.grid(row=10, column=0, padx=10, pady=0)
         api_Key = customtkinter.CTkEntry(self.sidebar_frame, placeholder_text="1234")
-        api_Key.grid(row=9, column=0, padx=20, pady=20)
+        api_Key.grid(row=11, column=0, padx=10, pady=0)
         api_Key.insert(0, 1234)
 
         port_label = customtkinter.CTkLabel(self.sidebar_frame, text="Network Port", fg_color="transparent")
-        port_label.grid(row=10, column=0, padx=20, pady=10)
+        port_label.grid(row=12, column=0, padx=10, pady=0)
         port = customtkinter.CTkEntry(self.sidebar_frame, placeholder_text="80")
-        port.grid(row=11, column=0, padx=20, pady=20)
+        port.grid(row=13, column=0, padx=10, pady=(0, 20))
         port.insert(0, 14053)
 
         # Configure suit frames
